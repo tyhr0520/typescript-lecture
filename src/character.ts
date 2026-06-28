8204e285fb1809bc31fb803f94c259ebb2ca4ef2
Sara_Toyohara <tyhr.0520@gmail.com>
2026-06-28 19:02:03 +0900

    attackの移動とisDeadの追加

--- src/character.ts
+++ src/character.ts
@@ -1,6 +1,7 @@
 export abstract class Character {
   protected name: string; // インスタンスからはアクセス出来ないが、継承先からはアクセスできる
-  private hp: number; // インスタンスからも継承先からもアクセス出来ない
+  protected hp: number;
+  //privateはインスタンスからも継承先からもアクセス出来ない
 
   constructor(name: string, hp: number) {
     this.name = name;
@@ -11,5 +12,17 @@ export abstract class Character {
     console.log(`${this.name}: HP ${this.hp}`);
   }
 
-  abstract attack(): void
+  public takeDamage(damage: number):void{
+    this.hp -= damage;
+
+    if(this.hp <0){
+      this.hp = 0;
+    }//残りHP以上のダメージを喰らったとき、マイナスにならないようにする（すべて０にする）
+  }
+
+  public isDead(): boolean {
+  return this.hp <= 0;
+  }
+
+  abstract attack(opponent: Character):void;
 }
